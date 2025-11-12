0ed479c8890cd02c8fd2318196297b6b8f888819
Raghav Kaushik Ravi <136466980+rraghavkaushik@users.noreply.github.com>
2025-11-13 00:52:43 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # EduBot Server Startup Script with Gemini API Key
 
-export GEMINI_API_KEY="AIzaSyAN8Hfz6TYALvH2Jp-R3aIG7Bp-ST6tcno"
+export GEMINI_API_KEY=""
 
 # Activate virtual environment
 source venv/bin/activate
